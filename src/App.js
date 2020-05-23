3c5d8c13694b207f43b6b396b983797d3bf0fcea
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-23 16:48:01 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from "react";
 import "./App.css";
+import Apod from "./components/Apod";
+import styled from "styled-components";
+
 
 function App() {
+  
+  const InnerContainer = styled.div `
+
+    width: 800px;
+
+  `
+  const OuterContainer = styled.div `
+
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    background-color: #7b8993;
+    color: white;
+
+    img{
+      border: 10px solid transparent;
+      padding: 15px;
+      border-image: url(https://www.w3schools.com/cssref/border.png) 50 round;
+    }
+
+    copyright {
+      text-decoration: underline;
+      word-spacing: length;
+    }
+
+  `
+  
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+    <OuterContainer>
+      <InnerContainer>
+        <div className="App">
+          <h1>
+            NASA's Photo of the Day<span role="img" aria-label='go!'>🚀</span>!
+          </h1>
+          <Apod />
+          <br></br>
+          <footer>
+            <a href="http://erikrai.com">@Supreme.Ciento</a> | Cloud Bots™
+          </footer><br />
+        </div>
+      </InnerContainer>
+    </OuterContainer>
   );
 }
 
