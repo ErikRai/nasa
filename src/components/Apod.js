ebbde8837d7bde7b46420edd6f3b4681ea165820
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-23 16:51:24 -0400

    Update Apod.js

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -5,6 +5,7 @@ import NASA from "./Card";
 import styled from "styled-components";
 
 export default function Apod(props) {
+
     const [url, setUrl] = useState("");
     const [urlType, seturlType] = useState("image");
     const [date, setDate] = useState("");
@@ -13,29 +14,45 @@ export default function Apod(props) {
     const [copyright, setCopyright] = useState("");
 
     const div = styled.section `
+
     width: 100%;
     border-radius: 10px;
+
     `;
 
     useEffect(() => {
+
       axios
+
         .get(
+
           `https://api.nasa.gov/planetary/apod?api_key=fXmhweTsDzm9oUGF871RBrvT5VT7Ib23X0Ero9sd`
+        
         )
+
         .then(res => {
+
             console.log(res);
             setUrl(res.data.url);
             seturlType(res.data.media_type);
             setDate(res.data.date);
             setExplanation(res.data.explanation);
             setTitle(res.data.title);
+
             if(res.data.hasOwnProperty("copyright")){
+
               setCopyright(res.data.copyright);
+
             }
+
           })
+
       }, [props.date]);
+
     return (
+
       <div>
+
         <NASA
           title={title}
           date={date}
@@ -44,6 +61,9 @@ export default function Apod(props) {
           copyright={copyright}
           explanation={explanation}
         />
+
       </div>
+
     );
+    
   }
