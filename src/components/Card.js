31ef746c8dc34a85f37f5b478889375e6fb17da7
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-23 16:47:59 -0400

    Create Card.js

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+/*import Media from "./Media";*/
+
+const NASA = ({ copyright, date, explanation, title, url }) => {
+  return (
+    <div>
+      <title>Title: {title}</title>
+      <date>Date: {date}</date>
+      <img src={url} alt='NASAs Photo'/>
+      <copyright> {copyright} </copyright>
+      <explanation> {explanation} </explanation>
+    </div>
+  );
+};
+
+export default NASA;
